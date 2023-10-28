2604f7c751f7207c8d51f0b5df501b7e62ec51fc
Kissu <142857872+Kissu1@users.noreply.github.com>
2023-10-28 15:13:16 +0530

    Update query.py

--- plugins/query.py
+++ plugins/query.py
@@ -432,10 +432,10 @@ async def cb_handler(client: Client, query: CallbackQuery):
             InlineKeyboardButton("➕️ Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Cʜᴀᴛ ➕", url=f"http://t.me/{temp.U_NAME}?startgroup=true")
             ],[
             InlineKeyboardButton("Sᴇᴀʀᴄʜ 🔎", switch_inline_query_current_chat=''), 
-            InlineKeyboardButton("Cʜᴀɴɴᴇʟ 🔈", url="https://t.me/devil_mods_official")
+            InlineKeyboardButton("𝐊𝐢𝐬𝐬𝐮 💞", url="https://t.me/kissu123456")
             ],[      
             InlineKeyboardButton("Hᴇʟᴩ 🕸️", callback_data="help"),
-            InlineKeyboardButton("Aʙᴏᴜᴛ ✨", callback_data="about")
+            InlineKeyboardButton("Aʙᴏᴜᴛ 🌱", callback_data="about")
         ]]
         await query.edit_message_media(InputMediaPhoto(random.choice(PICS), START_MESSAGE.format(user=query.from_user.mention, bot=client.mention), enums.ParseMode.HTML), reply_markup=InlineKeyboardMarkup(buttons))
        
